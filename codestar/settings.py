2282adfa357272451da9678da7c66d7cf75a3b06
gregcotech007 <commins.greg@outlook.com>
2022-06-16 10:08:45 +0000

    Check 404 error on Heroku deployment Debug

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ DEBUG = True
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ["gregcotech007-codestar.herokuapp.com/", "localhost"]
+ALLOWED_HOSTS = ['gregcotech007-codestar.herokuapp.com', 'localhost']
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
